af44b1dbd35a46a9d7552f8ce1a573e6b54f1a6e
lottiger <linda.lottiger@student.kyh.se>
2024-10-17 14:51:21 +0200

    adds star

--- app/(root)/_components/apartment-list.tsx
+++ app/(root)/_components/apartment-list.tsx
@@ -44,8 +44,16 @@ const ApartmentList = (): JSX.Element => {
           {/* Titel och rating */}
           <div className="flex justify-between font-semibold mt-2">
             <h3>{apartment.titel}</h3>
+            <div className='flex items-center'>
+    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M2.9125 11L3.725 7.4875L1 5.125L4.6 4.8125L6 1.5L7.4 4.8125L11 5.125L8.275 7.4875L9.0875 11L6 9.1375L2.9125 11Z" fill="#1D1B20"/>
+</svg>
+<p>4.8</p>
+</div>
+
           </div>
 
+         
           {/* Sovrum och sängar */}
           <div className="flex items-center gap-2 text-gray-600">
             <p>{apartment.bedrooms} sovrum</p>
