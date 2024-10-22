b9ce0b4edb567f775b1c290bc7f312bd506bdc56
lottiger <linda.lottiger@student.kyh.se>
2024-10-22 16:00:53 +0200

    adds styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import { ClientProviders } from "./(root)/_components/client-providers";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
 
 const inter = Inter ({ subsets: ["latin"] });
 
