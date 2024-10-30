7449021e5c689129e21f86754209cbefb9c5e64c
lottiger <linda.lottiger@student.kyh.se>
2024-10-30 14:48:49 +0100

    adds guest-selector in booking-card

--- app/(root)/_components/guest-selector.tsx
+++ app/(root)/_components/guest-selector.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useGuestContext } from '@/context/guest-context';
+import { PlusIcon } from './plus-icon';
+import MinusIcon from './minus-icon';
 
 interface GuestSelectorProps {
   onClose: () => void; // Prop för att stänga GuestSelector
@@ -32,18 +34,14 @@ const GuestSelector: React.FC<GuestSelectorProps> = ({ onClose }) => {
           <button 
             onClick={() => { setAdults(adults > 0 ? adults - 1 : 0); }}
           >
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M3 7H11M13.5 7C13.5 8.72391 12.8152 10.3772 11.5962 11.5962C10.3772 12.8152 8.72391 13.5 7 13.5C5.27609 13.5 3.62279 12.8152 2.40381 11.5962C1.18482 10.3772 0.5 8.72391 0.5 7C0.5 5.27609 1.18482 3.62279 2.40381 2.40381C3.62279 1.18482 5.27609 0.5 7 0.5C8.72391 0.5 10.3772 1.18482 11.5962 2.40381C12.8152 3.62279 13.5 5.27609 13.5 7Z" stroke="black" />
-            </svg>
+            <MinusIcon />
           </button>
           <span className='px-[22px]'>{adults}</span>
           <button 
             onClick={() => { if (totalGuests < maxGuests) setAdults(adults + 1); }}
             disabled={totalGuests >= maxGuests}
           >
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M7 0C3.13403 0 0 3.13403 0 7C0 10.8662 3.13403 14 7 14C10.8662 14 14 10.8662 14 7C14 3.13403 10.8662 0 7 0ZM7 13.1388C3.62272 13.1388 0.875 10.3773 0.875 6.99997C0.875 3.62269 3.62272 0.874973 7 0.874973C10.3773 0.874973 13.125 3.6227 13.125 6.99997C13.125 10.3772 10.3773 13.1388 7 13.1388ZM10.0625 6.5625H7.4375V3.9375C7.4375 3.696 7.2415 3.5 7 3.5C6.7585 3.5 6.5625 3.696 6.5625 3.9375V6.5625H3.9375C3.696 6.5625 3.5 6.7585 3.5 7C3.5 7.2415 3.696 7.4375 3.9375 7.4375H6.5625V10.0625C6.5625 10.304 6.7585 10.5 7 10.5C7.2415 10.5 7.4375 10.304 7.4375 10.0625V7.4375H10.0625C10.304 7.4375 10.5 7.2415 10.5 7C10.5 6.7585 10.304 6.5625 10.0625 6.5625Z" fill="black" />
-            </svg>
+           <PlusIcon />
           </button>
         </div>
       </div>
@@ -59,18 +57,14 @@ const GuestSelector: React.FC<GuestSelectorProps> = ({ onClose }) => {
             onClick={() => { setChildren(children > 0 ? children - 1 : 0); }} 
             disabled={children <= 0}
           >
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M3 7H11M13.5 7C13.5 8.72391 12.8152 10.3772 11.5962 11.5962C10.3772 12.8152 8.72391 13.5 7 13.5C5.27609 13.5 3.62279 12.8152 2.40381 11.5962C1.18482 10.3772 0.5 8.72391 0.5 7C0.5 5.27609 1.18482 3.62279 2.40381 2.40381C3.62279 1.18482 5.27609 0.5 7 0.5C8.72391 0.5 10.3772 1.18482 11.5962 2.40381C12.8152 3.62279 13.5 5.27609 13.5 7Z" stroke="black" />
-            </svg>
+            <MinusIcon />
           </button>
           <span className='px-[22px]'>{children}</span>
           <button 
             onClick={() => { if (totalGuests < maxGuests) setChildren(children + 1); }}
             disabled={totalGuests >= maxGuests}
           >
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M7 0C3.13403 0 0 3.13403 0 7C0 10.8662 3.13403 14 7 14C10.8662 14 14 10.8662 14 7C14 3.13403 10.8662 0 7 0ZM7 13.1388C3.62272 13.1388 0.875 10.3773 0.875 6.99997C0.875 3.62269 3.62272 0.874973 7 0.874973C10.3773 0.874973 13.125 3.6227 13.125 6.99997C13.125 10.3772 10.3773 13.1388 7 13.1388ZM10.0625 6.5625H7.4375V3.9375C7.4375 3.696 7.2415 3.5 7 3.5C6.7585 3.5 6.5625 3.696 6.5625 3.9375V6.5625H3.9375C3.696 6.5625 3.5 6.7585 3.5 7C3.5 7.2415 3.696 7.4375 3.9375 7.4375H6.5625V10.0625C6.5625 10.304 6.7585 10.5 7 10.5C7.2415 10.5 7.4375 10.304 7.4375 10.0625V7.4375H10.0625C10.304 7.4375 10.5 7.2415 10.5 7C10.5 6.7585 10.304 6.5625 10.0625 6.5625Z" fill="black" />
-            </svg>
+            <PlusIcon />
           </button>
         </div>
       </div>
@@ -86,18 +80,14 @@ const GuestSelector: React.FC<GuestSelectorProps> = ({ onClose }) => {
             onClick={() => { setInfants(infants > 0 ? infants - 1 : 0); }} 
             disabled={infants <= 0}
           >
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M3 7H11M13.5 7C13.5 8.72391 12.8152 10.3772 11.5962 11.5962C10.3772 12.8152 8.72391 13.5 7 13.5C5.27609 13.5 3.62279 12.8152 2.40381 11.5962C1.18482 10.3772 0.5 8.72391 0.5 7C0.5 5.27609 1.18482 3.62279 2.40381 2.40381C3.62279 1.18482 5.27609 0.5 7 0.5C8.72391 0.5 10.3772 1.18482 11.5962 2.40381C12.8152 3.62279 13.5 5.27609 13.5 7Z" stroke="black" />
-            </svg>
+           <MinusIcon />
           </button>
           <span className='px-[22px]'>{infants}</span>
           <button 
             onClick={() => { if (totalGuests < maxGuests) setInfants(infants + 1); }}
             disabled={totalGuests >= maxGuests}
           >
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M7 0C3.13403 0 0 3.13403 0 7C0 10.8662 3.13403 14 7 14C10.8662 14 14 10.8662 14 7C14 3.13403 10.8662 0 7 0ZM7 13.1388C3.62272 13.1388 0.875 10.3773 0.875 6.99997C0.875 3.62269 3.62272 0.874973 7 0.874973C10.3773 0.874973 13.125 3.6227 13.125 6.99997C13.125 10.3772 10.3773 13.1388 7 13.1388ZM10.0625 6.5625H7.4375V3.9375C7.4375 3.696 7.2415 3.5 7 3.5C6.7585 3.5 6.5625 3.696 6.5625 3.9375V6.5625H3.9375C3.696 6.5625 3.5 6.7585 3.5 7C3.5 7.2415 3.696 7.4375 3.9375 7.4375H6.5625V10.0625C6.5625 10.304 6.7585 10.5 7 10.5C7.2415 10.5 7.4375 10.304 7.4375 10.0625V7.4375H10.0625C10.304 7.4375 10.5 7.2415 10.5 7C10.5 6.7585 10.304 6.5625 10.0625 6.5625Z" fill="black" />
-            </svg>
+           <PlusIcon />
           </button>
         </div>
       </div>
--- app/(root)/details/[id]/_components/booking-card.tsx
+++ app/(root)/details/[id]/_components/booking-card.tsx
@@ -3,6 +3,7 @@ import { ArrowIcon } from './arrow-icon';
 import { useGuestContext } from '@/context/guest-context';
 import { useDateContext } from '@/context/date-context';
 import DatePicker from '@/app/(root)/_components/date-picker';
+import GuestSelector from '@/app/(root)/_components/guest-selector';
 
 
 interface BookingCardProps {
@@ -13,13 +14,20 @@ const BookingCard: React.FC<BookingCardProps> = ({ pricePerNight }) => {
   const { guests } = useGuestContext();
   const { checkInDate, checkOutDate } = useDateContext();
   const [isDatePickerVisible, setIsDatePickerVisible] = useState(false);
+  const [isGuestSelectorVisible, setIsGuestSelectorVisible] = useState(false);
 
   const datePickerRef = useRef<HTMLDivElement>(null);
+  const guestSelectorRef = useRef<HTMLDivElement>(null);
 
+  // Stänger DatePicker och GuestSelector när man klickar utanför dem
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (datePickerRef.current && !datePickerRef.current.contains(event.target as Node)) {
+      const target = event.target as Node;
+      if (datePickerRef.current && !datePickerRef.current.contains(target)) {
         setIsDatePickerVisible(false);
+      }
+      if (guestSelectorRef.current && !guestSelectorRef.current.contains(target)) {
+        setIsGuestSelectorVisible(false);
       }
     };
     document.addEventListener('mousedown', handleClickOutside);
@@ -38,9 +46,16 @@ const BookingCard: React.FC<BookingCardProps> = ({ pricePerNight }) => {
 
   const totalPrice = nights * pricePerNight;
 
+  // Hanterar formsubmission
+  const handleReservation = (event: React.FormEvent) => {
+    event.preventDefault();
+    // Lägg till bokningslogik här
+    console.log("Bokning genomförd!");
+  };
+
   return (
     <div className="w-[300px] h-[351px] shadow-md p-4 ml-10 flex justify-center">
-      <form>
+      <form onSubmit={handleReservation}>
         <div className="text-[12px] font-semibold mt-4">
           <div className="flex" onClick={() => setIsDatePickerVisible(!isDatePickerVisible)}>
             <div className="border w-[122px] px-2 py-1 rounded-tl cursor-pointer">
@@ -57,7 +72,8 @@ const BookingCard: React.FC<BookingCardProps> = ({ pricePerNight }) => {
               <DatePicker />
             </div>
           )}
-          <div className="flex justify-between border-x border-b w-[244px] py-1 px-2 rounded-b">
+
+          <div className="flex justify-between border-x border-b w-[244px] py-1 px-2 rounded-b" onClick={() => setIsGuestSelectorVisible(!isGuestSelectorVisible)}>
             <div>
               <p>Antal</p>
               <p>{guests > 0 ? `${guests} gäster` : 'Välj antal'}</p>
@@ -66,6 +82,12 @@ const BookingCard: React.FC<BookingCardProps> = ({ pricePerNight }) => {
               <ArrowIcon />
             </div>
           </div>
+          {isGuestSelectorVisible && (
+            <div ref={guestSelectorRef} className="absolute z-50 bg-white rounded shadow-md mt-2">
+              <GuestSelector onClose={() => setIsGuestSelectorVisible(false)} />
+            </div>
+          )}
+
           <div className="flex justify-center items-center flex-col text-center">
             <div className="font-normal text-gray-500 mt-4">
               <p>{nights} {nights === 1 ? 'dygn' : 'dygn'}</p>
@@ -79,7 +101,7 @@ const BookingCard: React.FC<BookingCardProps> = ({ pricePerNight }) => {
               />
               <p className="mt-4">Totalt: {totalPrice} kr</p>
               <p className="text-[11px] font-normal text-gray-500">inklusive skatter och avgifter</p>
-              <button className="mt-4 bg-[#1E3E62] text-white rounded-md py-3 px-20 text-[16px]">
+              <button type="submit" className="mt-4 bg-[#1E3E62] text-white rounded-md py-3 px-20 text-[16px]">
                 Reservera
               </button>
             </div>
