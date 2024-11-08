b68a150c6360db2eca5b472e798d065d846fb10a
lottiger <linda.lottiger@student.kyh.se>
2024-11-08 09:45:07 +0100

    adds res searchbar

--- app/(root)/bookings/page.tsx
+++ app/(root)/bookings/page.tsx
@@ -1,11 +1,14 @@
+
 import React from 'react'
 import BookingsList from './booking-list'
 import HeaderWithoutSearch from '../_components/header-without-search'
+
 
 function MyBookingsPage(): JSX.Element {
   return (
     <>
     <HeaderWithoutSearch />
+  
     <h1 className='text-[40px] font-semibold flex justify-center my-20'>Bokningshistorik</h1>
     <BookingsList />
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
   	extend: {
+		screens: {
+			'smx': '800px',
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
